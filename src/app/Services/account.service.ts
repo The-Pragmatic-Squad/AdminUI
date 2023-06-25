dff2a2ed6ee3bf8ab8015e60d739ae2deca72593
Muhammad <21185758+Muhammad-Saad-01@users.noreply.github.com>
2023-06-25 18:27:14 +0300

    Fix all accounts routing in add account

--- src/app/Services/account.service.ts
+++ src/app/Services/account.service.ts
@@ -1,29 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { enviroment } from 'src/enviroments/enviroment';
 import { Account } from '../model/account';
 import { Transaction } from '../model/transaction';
 import { AccountDetailed } from '../model/accountdetailed';
 // import * as data from '../model/accounts.json'
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AccountService {
-    private url: string = enviroment.BaseURLApi;
-    constructor(private http: HttpClient) {
-    }
-    getAllAccounts(): Observable<Account[]> {
-      return this.http.get<Account[]>(this.url);
-    }
-    getAccountById(id: number): Observable<Account>{
-      return this.http.get<Account>(`${this.url}/avatar/${id}`);
-    }
-    getTransactionDetailsByAccountId(id: number):Observable<Transaction[]>{
-      return this.http.get<Transaction[]>(`${this.url}/${id}/transactions`);
-    }
-    createAccount(account: AccountDetailed):Observable<number>{
-      return this.http.post<number>(`${this.url}`,account);
-    }     
+  private url: string = enviroment.BaseURLApi;
+  constructor(private http: HttpClient) {
+  }
+  getAllAccounts(): Observable<Account[]> {
+    return this.http.get<Account[]>(this.url);
+  }
+  getAccountById(id: number): Observable<Account> {
+    return this.http.get<Account>(`${this.url}/avatar/${id}`);
+  }
+  getTransactionDetailsByAccountId(id: number): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(`${this.url}/${id}/transactions`);
+  }
+  createAccount(account: AccountDetailed): Observable<number> {
+    // return this.http.post<number>(`${this.url}`, account);
+    return this.http.post<number>(`${this.url}`, account);
+
+
+
+  }
 }
